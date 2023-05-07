b448aa890654c69db38786f844c00caab7fd584b
rhsabbir1 <rhsabbir090@gmai.com>
2023-05-07 21:29:46 +0600

    post data server

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
-import { Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
 
+  const handleAddUser = event =>{
+    event.preventDefault()
+    const from = event.target;
+    const name = from.name.value;
+    const email = from.email.value;
 
+    const user = {name , email}
+    fetch('http://localhost:5000/users',{
+      method:"POST",
+      headers:{
+        'content-type':'application/json'
+      },
+      body:JSON.stringify(user)
+    })
+    .then(res => res.json())
+    .then(data=> {
+      console.log(data)
+    })
+    console.log(user)
+  }
 
 
   return (
     <>
-     <Outlet></Outlet>
+
+      <h1>Simple CRUD</h1>
+
+      <form onSubmit={handleAddUser}>
+        <input type="text" name='name' placeholder='Name' />
+        <br />
+        <input type="email" name="email" id="" placeholder='Email' />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
+     
     </>
   )
 }
